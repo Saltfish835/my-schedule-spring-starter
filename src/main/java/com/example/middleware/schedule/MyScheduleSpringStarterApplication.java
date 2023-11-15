b2dc3cccc865d1ceede4b3299fd4ca306d0bfb22
yuhe <yuhe2@intra.nsfocus.com>
2023-11-15 16:35:07 +0800

    初始提交

---
+++ src/main/java/com/example/middleware/schedule/MyScheduleSpringStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.middleware.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScheduleSpringStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyScheduleSpringStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/middleware/schedule/MyScheduleSpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.middleware.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScheduleSpringStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
